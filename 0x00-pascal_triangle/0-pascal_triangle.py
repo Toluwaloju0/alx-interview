913e2eaa9c30bdd6141133b84c1ea0273163c569
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-04 13:13:10 +0100

    alx-interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,8 +14,8 @@ def pascal_triangle(n):
     triangle = []
     for i in range(n):
         triangle.append([])
-        for j in range(i):
-            if j == 0 or j == i - 1:
+        for j in range(i + 1):
+            if j == 0 or j == i:
                 triangle[i].append(1)
             else:
                 triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
