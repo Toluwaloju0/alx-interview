62a34e53325564f9fb7f0c0643e44ef08d23d1ae
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-08 18:00:04 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A module to create a lock box function"""
+
+
+def canUnlockAll(boxes):
+    """A function to check if all the boxes can be unlocked
+    Args:
+        boxes(list of list): containint=g the keys for the box
+    return: True if all boxes cn be unlocked else False
+    """
+
+    checker = set([0])
+    newList = []
+    
+    # Iterate over the boxes, first check if the key is in the set before checking it
+    for box in range(len(boxes)):
+        if box in checker:
+            for key in boxes[box]:
+                checker.add(key)
+                if key < box:
+                     for P_key in boxes[key]:
+                        checker.add(P_key)
+        else:
+            newList.append(box)
+
+    final = []  
+    # Using newList check for missing keys
+    for key in newList:
+        if key not in checker:
+            final.append(key)
+
+    if len(final) > 0:
+        return False
+    return True
