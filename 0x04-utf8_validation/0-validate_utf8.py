51e06712c3423309729929229614fa888522adec
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-01 21:56:02 +0100

    utf validate

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module to validate utf-8 content"""
+
+import sys
+
+def validUTF8(data):
+    """A function to validate utf-8 contents"""
+    # iterate each number and shift the bits to the last 2
+    for char in data:
+        num  = char >> 6
+        if num > 3:
+            return False
+
+    return True
