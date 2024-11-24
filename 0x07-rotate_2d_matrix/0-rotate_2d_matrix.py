ecd54ef6b279851a9d98cf3ffa7987a9f2161922
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-24 16:41:33 +0100

    alx-interview 0-rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module to rotate pieces of a matrix"""
+
+
+def rotate_2d_matrix(matrix: list[list]) -> None:
+    """A list of list to be rotated"""
+
+    if matrix is None:
+        return None
+    length = len(matrix)
+
+    # create a new matrix and set 
+    new_mat = [[0] * length for _ in range(length)]
+    for a in range(len(matrix)):
+        for b in range(len(matrix)):
+            new_mat[b][length - 1] = matrix[a][b]
+        length -= 1
+
+    for a in range(len(matrix)):
+        for b in range(len(matrix)):
+            matrix[a][b] = new_mat[a][b]
