acc574b260ba62b5cbe615cf3fa78f0a14489a90
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-26 17:02:23 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module to create the making change"""
+
+
+def makeChange(coins, total):
+    """A function to implement the making change algorithm"""
+
+    if total <= 0:
+        return 0
+
+    given_coin = 0
+    coins.sort(reverse=True)
+    while total > 0:
+        if total < coins[-1]:
+            return -1
+        # iterate through the coins to find the
+        # highest coin in respect to total
+        for coin in coins:
+            if total >= coin:
+                total = total - coin
+                given_coin += 1
+                break
+    return given_coin
