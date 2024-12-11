04db06c0b207d5348781dea66166dc89bd606f48
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-11 21:15:39 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A module to get the winner of a prime game"""
+
+
+def is_prime(num):
+    """To check if a nymber is a prime number
+    """
+
+    if num == 0 or num == 1:
+        return True
+    for div in range(2, num):
+        if num % div == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    ben_score = 0
+    maria_score = 0
+
+    for num in nums:
+        x -= 1
+        game_score = 0
+        while num > 0:
+            if is_prime(num):
+                game_score += 1
+            num -= 1
+        # if game_score is odd Ben wins else Maria wins
+        if game_score % 2 == 1:
+            ben_score += 1
+        else:
+            maria_score += 1
+        if x == 0:
+            break
+    if ben_score > maria_score or maria_score == 0:
+        return "Ben"
+    return "Maria"
